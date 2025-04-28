2edd7f5a7c3685a6406d90b7487e5069ef248905
Romain Lechêne <romain_lechene@ens.univ-artois.fr>
2025-04-28 11:11:18 +0200

    Updated components

--- itemization-tool/src/app/app.component.ts
+++ itemization-tool/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TeamSelectorComponent} from './components/team-selector/team-selector.component';
+import {ChampionPromptComponent} from './components/champion-prompt/champion-prompt.component';
+import {ItemShopComponent} from './components/item-shop/item-shop.component';
+import {MinimapComponent} from './components/minimap/minimap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TeamSelectorComponent, ChampionPromptComponent, ItemShopComponent, MinimapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
