ccd58a72ab56479dcb27e7160378746cc1141ecb
Romain Lechêne <romain_lechene@ens.univ-artois.fr>
2025-05-05 13:56:17 +0200

    Added skeleton of the component : searchbar + items slots + functions to make it all work (doesn't work yet yikes)

--- itemization-tool/src/app/components/item-shop/item-shop.component.ts
+++ itemization-tool/src/app/components/item-shop/item-shop.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {ChampionsService} from '../../champions.service';
+import {ItemsService} from '../../items.service';
+import {Item} from '../../item';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-item-shop',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './item-shop.component.html',
   styleUrl: './item-shop.component.scss'
 })
 export class ItemShopComponent {
+  choiceValidated = signal(false);
+  searchControl = new FormControl('');
+  selectedSlot = signal<number | null>(null);
+  selectedItems = signal<Item[]>(Array(6).fill(null));
+  allItems;
+
+  filteredItems = computed(() => {
+    const query = this.searchControl.value?.toLowerCase() || '';
+    return this.allItems().filter(item =>
+    item.name.toLowerCase().includes(query)
+    );
+  });
+
+  constructor(private championService: ChampionsService, itemService: ItemsService) {
+    this.choiceValidated = championService.choiceValidated;
+    this.allItems = itemService.items;
+  }
+
+  onItemClicked(item: Item){
+    const slot = this.selectedSlot();
+    if (slot !== null){
+      const updated = [...this.selectedItems()];
+      updated[slot] = item;
+      this.selectedItems.set(updated);
+      this.selectedSlot.set(null);
+    } else{
+      const updated = [...this.selectedItems()];
+      const firstEmpty = updated.findIndex(i => i === null);
+      if (firstEmpty !== -1){
+        updated[firstEmpty] = item;
+        this.selectedItems.set(updated);
+      }
+    }
+  }
+
+  onSlotClicked(index: number){
+    if (this.selectedSlot() === index){
+      this.selectedSlot.set(null);
+    } else{
+      this.selectedSlot.set(index);
+    }
+  }
 
 }
